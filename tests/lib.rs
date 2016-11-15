0bb45b9fb78a5a955d56f20c10d0bcef527e3265
Alexandre Oliveira <rockytvbr@gmail.com>
2016-11-15 13:35:15 -0200

    Add artist inequality test

--- tests/lib.rs
+++ tests/lib.rs
@@ -23,3 +23,27 @@ fn test_artist_equal() {
 
     assert_eq!(a, b);
 }
+
+#[test]
+#[should_panic]
+fn test_artist_not_equal() {
+    let a = artist::Artist::new(
+        Uuid::nil(),
+        String::from("foo"),
+        String::new(),
+        enums::PersonType::Other,
+        Vec::new(),
+        Vec::new()
+    );
+
+    let b = artist::Artist::new(
+        Uuid::nil(),
+        String::from("bar"),
+        String::from("gender"),
+        enums::PersonType::Other,
+        vec![String::from("tags")],
+        Vec::new(),
+    );
+
+    assert_eq!(a, b);
+}
